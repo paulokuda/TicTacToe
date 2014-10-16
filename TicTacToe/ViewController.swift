cfb03add0402e499fe2c693b4b3af22f4fabdf83
Paul <paulyokuda@gmail.com>
2014-10-16 10:43:15 -0400

    initial push

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var square1: UIImageView!
+    @IBOutlet var square2: UIImageView!
+    @IBOutlet var square3: UIImageView!
+    @IBOutlet var square4: UIImageView!
+    @IBOutlet var square5: UIImageView!
+    @IBOutlet var square6: UIImageView!
+    @IBOutlet var square7: UIImageView!
+    @IBOutlet var square8: UIImageView!
+    @IBOutlet var square9: UIImageView!
+
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    @IBOutlet var button5: UIButton!
+    @IBOutlet var button6: UIButton!
+    @IBOutlet var button7: UIButton!
+    @IBOutlet var button8: UIButton!
+    @IBOutlet var button9: UIButton!
+    
+    
+    @IBOutlet var resetButton: UIButton!
+    @IBOutlet var messageLabel: UILabel!
+    
+    
+    @IBAction func UIButtonClicked(sender:UIButton) {
+        messageLabel.hidden = true
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
